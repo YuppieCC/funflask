60fce60c01f5f5ea84aba391d3b2f55f26f240b4
EacentCheung <eacentcheung@gmail.com>
2017-02-06 21:12:45 +0800

    HTTPAuth

--- app/models.py
+++ app/models.py
@@ -97,6 +97,20 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return 'User: %r' % self.username
+    
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=expiration)
+        return s.dumps({'id': self.id})
+    
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.condig['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
 
     @staticmethod
     def add_self_follows():
